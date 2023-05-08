17930e4355ca0b85eb8afbd2cf5ccfa4e568bbcf
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-05-08 16:57:01 +0530

    Modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-import { MoralisProvider } from "react-moralis";
-import { NotificationProvider } from "web3uikit";
-import "../styles/globals.css";
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <Component {...pageProps} />
-      </NotificationProvider>
-    </MoralisProvider>
-  );
+export default function App({ Component, pageProps }) {
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
+    )
 }
-
-export default MyApp;
